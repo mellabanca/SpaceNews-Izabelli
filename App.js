45ff18a74fcac226a9a1dacf935440cc1faf2f17
mellabanca <mellabanca@gmail.com>
2022-11-11 13:50:14 -0300

    aula 76 deu boa

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from "./screens/Home";
+import MeteorScreen from "./screens/Meteors";
+import ISSLocationScreen from "./screens/ISSLocation";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home"
+                       screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="ISSLocation" component={ISSLocationScreen}/>
+        <Stack.Screen name="Meteors" component={MeteorScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/ISSLocation.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {Text, View} from "react-native";
+
+export default class ISSLocationScreen extends Component{
+    render(){
+        return(
+            <View style ={{flex: 1,
+                           justifyContent: "center",
+                           alignItems: "center"}}>
+                <Text>Tela da localização da EEI</Text>
+            </View>
+        )
+    }
+}
