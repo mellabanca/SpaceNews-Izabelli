9263a0a76cab9a384bc3708341bee367348b7361
mellabanca <mellabanca@gmail.com>
2022-12-07 13:46:01 -0300

    Aula 79 calculo meteoros

--- screens/Meteors.js
+++ screens/Meteors.js
@@ -1,8 +1,55 @@
 import React, {Component} from "react";
-import {Text, View} from "react-native";
+import {Alert, Text, View} from "react-native";
 
 export default class MeteorScreen extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            meteors: {}
+        }
+    }
+
+    componentDidMount(){
+        this.getMeteors();
+    }
+
+    getMeteors = () => {
+        axios
+             .get("https://api.nasa.gov/neo/rest/v1/feed?api_key=gRsH9ySDkr2gVQjlpSJI3SEyym60hyQWXsMhRbTa")
+             .then(response=> {
+                this.setState({
+                    meteors: response.data.near_earth_objects
+                })
+             })
+             .catch(error=>{
+                Alert.alert(error.message)
+             })
+    }
+
     render(){
+        if(Object.keys(this.state.meteors).length === 0){
+            return(
+                <View style = {styles.loadingLocation}>
+                    <Text>Carrregando...</Text>
+                </View>
+            )
+        }
+        else {
+            let meteor_arr = Object.keys(this.state.meteors).map(meteor_date => {
+                return this.state.meteors[meteor_date]
+            })
+            let meteors = [].concat.apply([],meteor_arr)
+
+            meteors.forEach(function(element){
+                let diameter =
+                (element.estimated_diameter.kilometers.estimated_diameter_min +
+                 element.estimated_diameter.kilometers.estimated_diameter_max) / 2;
+                let threatScore = 
+                (diameter / element.close_approach_data[0].miss_distance.kilometers)
+                * 100000000
+                element.threat_Score = threatScore
+            })
+
         return(
             <View style ={{flex: 1,
                            justifyContent: "center",
@@ -11,4 +58,5 @@ export default class MeteorScreen extends Component{
             </View>
         )
     }
+}
 }
